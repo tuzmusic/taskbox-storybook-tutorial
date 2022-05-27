80d2fc58ea425a096f6097ed1b035f88f468d8f5
Jonathan Tuzman <tuzmusic@gmail.com>
2022-05-27 14:39:41 -0400

    Flesh out task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,23 @@
 const Task = ({ task, onArchiveTask, onPinTask }) => {
   const { id, title, state } = task;
 
-  return <div className="list-item">
-    <input type="text" value={title} readOnly/>
+  return <div className={`list-item ${state}`}>
+    <label className="checkbox">
+      <input type="checkbox" defaultChecked={state === "TASK_ARCHIVED"} disabled name="checked"/>
+      <span className="checkbox-custom" onClick={() => onArchiveTask(id)} id={`archiveTask-${id}`}
+            aria-label={`archiveTask-${id}`}/>
+    </label>
+    <div className="title">
+      <input type="text" value={title} readOnly/>
+    </div>
+
+    <div className={"actions"} onClick={event => event.stopPropagation()}>
+      {state !== "TASK_ARCHIVED" &&
+        // eslint-disable-next-line jsx-a11y/anchor-is-valid
+        <a onClick={() => onPinTask(id)}>
+          <span className={`icon-star`} id={`pinTask-${id}`} aria-label={`pinTask-${id}`}/>
+        </a>}
+    </div>
   </div>
 }
 
